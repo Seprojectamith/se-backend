6d5be681b46857019d8dc45533265b97fd5d4de4
Seprojectamith <160088671+Seprojectamith@users.noreply.github.com>
2024-02-15 09:48:20 -0600

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.mongodb_url, {
+mongoose.connect("mongodb+srv://seproject65:1234567890@cluster0.kn2b4wz.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
